40d436795212540d3f88232abf8b82c57937a6a8
lazarevaak (Лазарева Александра) <lazarevaak@yandex.ru>
2025-10-21 17:00:18 +0300

    Initial Commit

---
+++ Focora/FocoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocoraApp.swift
+//  Focora
+//
+//  Created by MacBoock on 21.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct FocoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
