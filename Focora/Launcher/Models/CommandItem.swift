ae2a540163d7a2ecb4012e61fe9ab775ec28d3f2
TEHEK1 <kasapovamir9@gmail.com>
2025-11-08 00:16:53 +0300

    Add application icons

--- Focora/Launcher/Models/CommandItem.swift
+++ Focora/Launcher/Models/CommandItem.swift
@@ -6,13 +6,31 @@
 //
 
 import Foundation
+import AppKit
 
 struct CommandItem: Identifiable, Hashable {
     let id = UUID()
     let icon: String
+    let appIcon: NSImage?
     let title: String
     let keywords: [String]
     let run: () -> Void
+    
+    init(icon: String, title: String, keywords: [String], run: @escaping () -> Void) {
+        self.icon = icon
+        self.appIcon = nil
+        self.title = title
+        self.keywords = keywords
+        self.run = run
+    }
+    
+    init(appIcon: NSImage?, title: String, keywords: [String], run: @escaping () -> Void) {
+        self.icon = ""
+        self.appIcon = appIcon
+        self.title = title
+        self.keywords = keywords
+        self.run = run
+    }
 
     static func == (lhs: CommandItem, rhs: CommandItem) -> Bool { lhs.id == rhs.id }
     func hash(into hasher: inout Hasher) { hasher.combine(id) }
